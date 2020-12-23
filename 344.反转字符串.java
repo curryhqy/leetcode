a176e59b52144ee35db134c4d75109adc6b8b2b2
curryhqy <1812584700@qq.com>
2020-12-23 16:42:28 +0800

    饭庄字符串

--- 344.反转字符串.java
+++ 344.反转字符串.java
@@ -7,7 +7,11 @@
 // @lc code=start
 class Solution {
     public void reverseString(char[] s) {
-
+        for (int i = 0; i < s.length / 2; i++) {
+            char temp = s[i];
+            s[i] = s[s.length - 1 - i];
+            s[s.length - 1 - i] = temp;
+        }
     }
 }
 // @lc code=end
