405601d40b705bdae9357fa39a0123f132edb166
curryhqy <1812584700@qq.com>
2020-12-25 09:37:28 +0800

    分发饼干

---
+++ 455.分发饼干.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode.cn id=455 lang=java
+ *
+ * [455] 分发饼干
+ */
+
+// @lc code=start
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int numOfChildren = g.length, numOfCookies = s.length;
+        int count = 0;
+        for (int i = 0, j = 0; i < numOfChildren && j < numOfCookies; i++, j++) {
+            while (j < numOfCookies && g[i] > s[j]) {
+                j++;
+            }
+            if (j < numOfCookies) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+// @lc code=end
+
