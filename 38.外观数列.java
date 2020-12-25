333bcaff9bd3908664cc549fc0d777025db49363
curryhqy <1812584700@qq.com>
2020-12-25 14:21:29 +0800

    外观数列

--- 38.外观数列.java
+++ 38.外观数列.java
@@ -7,7 +7,25 @@
 // @lc code=start
 class Solution {
     public String countAndSay(int n) {
-        
+        //递归终止条件
+        if (n == 1) {
+            return "1";
+        }
+        StringBuffer res = new StringBuffer();
+        //拿到上一层的字符串
+        String str = countAndSay(n - 1);
+        int len = str.length();
+        //指针读取字符串
+        int start = 0;
+        for (int i = 1; i < len + 1; i++) {
+            if (i == len) {
+                res.append(i - start).append(str.charAt(start));
+            } else if (str.charAt(i) != str.charAt(start)) {
+                res.append(i - start).append(str.charAt(start));
+                start = i;
+            }
+        }
+        return res.toString();
     }
 }
 // @lc code=end
