931cbd4d60dfae9dc3b5aa7ad88068cc10a33d26
curryhqy <1812584700@qq.com>
2020-12-25 17:59:08 +0800

    回文链表

---
+++ 234.回文链表.java
@@ -0,0 +1,68 @@
+
+/*
+ * @lc app=leetcode.cn id=234 lang=java
+ *
+ * [234] 回文链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null) {
+            return true;
+        }
+
+        //前半部分与后半部分比较即可
+        ListNode firstHalf = middle(head);
+        ListNode half = reverseList(firstHalf.next);
+
+        ListNode p1 = head;
+        ListNode p2 = half;
+        boolean result = true;
+        while (result && p2 != null) {
+            if (p1.val != p2.val) {
+                result = false;
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        //还原链表
+        firstHalf.next = reverseList(half);
+        return result;
+
+    }
+    //reverse
+    private ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        return pre;
+    }
+
+    //find middle
+    private ListNode middle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
+// @lc code=end
+
