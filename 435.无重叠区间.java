1290964458d3e52bcbe141bd3c50dd74c7ad6046
curryhqy <1812584700@qq.com>
2021-01-03 23:56:08 +0800

    分隔链表

---
+++ 435.无重叠区间.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+/*
+ * @lc app=leetcode.cn id=435 lang=java
+ *
+ * [435] 无重叠区间
+ */
+
+// @lc code=start
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        if (intervals.length == 0) {
+            return 0;
+        }
+        
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            public int compare (int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+
+        int n = intervals.length;
+        int[] f = new int[n];
+        Arrays.fill(f, 1);
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (intervals[j][1] <= intervals[i][0]) {
+                    f[i] = Math.max(f[i], f[j] + 1);
+                }
+            }
+        }
+        return n - Arrays.stream(f).max().getAsInt();
+    }
+}
+// @lc code=end
+
