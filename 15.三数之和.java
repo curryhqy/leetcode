fdf202e7d4c3926490bdfab38d4050f4a1b0ecc6
curryhqy <1812584700@qq.com>
2021-01-05 16:12:32 +0800

    三数之和优化

--- 15.三数之和.java
+++ 15.三数之和.java
@@ -13,7 +13,41 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         int n = nums.length;
         Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        //枚举a
+        for (int first = 0; first < n; first++) {
+            //需要和上次枚举的数不相同
+            if (first > 0 && nums[first] == nums[first - 1]) {
+                continue;
+            }
+            // c对应的指针指向数组最右端
+            int third = n - 1;
+            int target = -nums[first];
+            //枚举b
+            for (int second = first + 1; second < n; second++) {
+                //需要和上次枚举的数不相同
+                if (second > first + 1 && nums[second] == nums[second - 1]) {
+                    continue;
+                }
+                //需要保证 b 指针在 c 右侧
+                while (second < third && nums[second] + nums[third] > target) {
+                    --third;
+                }
+                //如果指针重合，再往后就会b > c
+                if (second == third) {
+                    break;
+                }
+                
+                if (nums[second] + nums[third] == target) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[first]);
+                    list.add(nums[second]);
+                    list.add(nums[third]);
+                    ans.add(list);
+                }
+            }
+        }
+        return ans;
     }
-    
 }
 // @lc code=end
