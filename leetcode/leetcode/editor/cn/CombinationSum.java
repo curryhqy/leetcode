d6a5a7902d05aad3621aa09767ea7e41584a0b20
curryhqy <1812584700@qq.com>
2021-10-24 01:08:36 +0800

    vscode to idea

---
+++ leetcode/leetcode/editor/cn/CombinationSum.java
@@ -0,0 +1,22 @@
+package editor.cn;
+
+import java.util.List;
+
+public class CombinationSum{
+    public static void main(String[] args) {
+        Solution solution = new CombinationSum().new Solution();
+        int[] a = new int[]{1, 3, 4};
+        System.out.println(solution.combinationSum(a , 2));
+        
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        System.out.println(0);
+        return null;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
