f02f04b94daec2b83aeafb914f2a7835ad1985dd
curryhqy <1812584700@qq.com>
2022-02-20 16:48:29 +0800

    20220220

--- leetcode/leetcode/editor/cn/OneBitAnd2BitCharacters.java
+++ leetcode/leetcode/editor/cn/OneBitAnd2BitCharacters.java
@@ -9,7 +9,12 @@ public class OneBitAnd2BitCharacters{
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean isOneBitCharacter(int[] bits) {
-
+        int len = bits.length;
+        int flag = 0;
+        while (flag < len - 1) {
+            flag += bits[flag] + 1;
+        }
+        return flag == len - 1;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
--- leetcode/leetcode/editor/cn/PairsWithSumLcci.java
+++ leetcode/leetcode/editor/cn/PairsWithSumLcci.java
@@ -1,5 +1,11 @@
 package editor.cn;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class PairsWithSumLcci{
     public static void main(String[] args) {
         Solution solution = new PairsWithSumLcci().new Solution();
@@ -9,7 +15,23 @@ public class PairsWithSumLcci{
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public List<List<Integer>> pairSums(int[] nums, int target) {
-
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        List<Integer> numList = Arrays.stream(nums).boxed().collect(Collectors.toList());
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            if (numList.get(left) + numList.get(right) == target) {
+                res.add(Arrays.asList(numList.get(left), numList.get(right)));
+                left++;
+                right--;
+            } else if (numList.get(left) + numList.get(right) < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return res;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
