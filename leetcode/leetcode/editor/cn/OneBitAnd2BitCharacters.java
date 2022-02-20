c3c8d98493e9d593619253e63e0b23094a5bdd41
curryhqy <1812584700@qq.com>
2022-02-20 16:48:04 +0800

    20220220

---
+++ leetcode/leetcode/editor/cn/OneBitAnd2BitCharacters.java
@@ -0,0 +1,17 @@
+package editor.cn;
+
+public class OneBitAnd2BitCharacters{
+    public static void main(String[] args) {
+        Solution solution = new OneBitAnd2BitCharacters().new Solution();
+        
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isOneBitCharacter(int[] bits) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ leetcode/leetcode/editor/cn/PairsWithSumLcci.java
@@ -0,0 +1,17 @@
+package editor.cn;
+
+public class PairsWithSumLcci{
+    public static void main(String[] args) {
+        Solution solution = new PairsWithSumLcci().new Solution();
+        
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<Integer>> pairSums(int[] nums, int target) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
